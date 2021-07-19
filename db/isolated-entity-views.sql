5d8c3c1e9ae8c3dc0b14da5bd5bace8e33b2c381
Stephen C. Harris <steveofar@hotmail.com>
2021-07-19 13:07:45 -0500

    Add isolated entity view example for ndc/sdc.

--- db/isolated-entity-views.sql
+++ db/isolated-entity-views.sql
@@ -16,3 +16,21 @@ select
 from scd
 join df on df.rxcui = scd.df_rxcui
 ;
+
+-- TODO: Define more drug related isolated entities above, then include in ndc_isoents_mv below.
+
+create materialized view ndc_isoents_mv as
+select
+  pc.two_part_ndc,
+  (select coalesce(json_agg(row_to_json(scd.*)), '[]'::json)
+   from isoent_scd_v scd
+   where scd.rxcui in (
+     select cd.scd_rxcui from ndc_scd_mv cd where cd.two_part_ndc = pc.two_part_ndc
+   )
+  ) scds
+from mthspl_prod_ndc pc
+join mthspl_prod p on p.rxaui = pc.prod_rxaui
+-- join
+group by pc.two_part_ndc
+;
+create unique index ix_ndcisoents_ndc on ndc_isoents_mv (two_part_ndc);
