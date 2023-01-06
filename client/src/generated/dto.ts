132144e58b2ad5e54f2040abeb856b88344acfb6
dan <dd482@njit.edu>
2023-01-06 12:47:38 -0500

    enable WPI

--- client/src/generated/dto.ts
+++ client/src/generated/dto.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.19.577 on 2021-07-19 15:50:40.
+// Generated using typescript-generator version 2.19.577 on 2022-12-27 17:26:13.
 
 export interface AppVersion {
     buildTimestamp?: Nullable<string>;
@@ -13,6 +13,22 @@ export interface AppVersion {
 export interface NdcRelatedEntities {
     ndc: string;
     scds: Scd[];
+    sbds: Sbd[];
+}
+
+export interface Sbd {
+    rxcui: string;
+    name: string;
+    prescribableName: string;
+    rxtermForm: string;
+    dosageForm: string;
+    availableStrengths: string;
+    qualDistinct: string;
+    quantity: string;
+    humanDrug: boolean;
+    vetDrug: boolean;
+    unquantifiedFormRxcui: string;
+    suppress: string;
 }
 
 export interface Scd {
---
+++ wpi-copy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# This script is a template for the WPI loop for a project with -Ainfer=ajava
+# added to its build file. Fill in the variables at the beginning of the
+# script with values that make sense for your project; the values there
+# now are examples.
+
+# Where should the output be placed at the end? This directory is also
+# used to store intermediate WPI results. The directory does not need to
+# exist. If it does exist when this script starts, it will be deleted.
+WPITEMPDIR=/tmp/WPITEMPDIR-RX
+
+# Where is WPI's output placed by the Checker Framework? This is some
+# directory ending in build/whole-program-inference. For most projects,
+# this directory is just ./build/whole-program-inference .
+# The example in this script is the output directory when running via the gradle plugin.
+# (The CF automatically puts all WPI outputs in ./build/whole-program-inference,
+# where . is the directory from which the javac command was invoked (ie, javac's
+# working directory). In many build systems (e.g., Maven), that directory would be the project.
+# But, some build systems, such as Gradle, cache build outputs in a central location
+# per-machine, and as part of that it runs its builds from that central location.)
+# The directory to use here might vary between build systems, between machines
+# (e.g., depending on your local Gradle settings), and even between projects using the
+# same build system (e.g., because of a project's settings.gradle file).
+#
+# TODO: When I use this with Gradle, wpi.sh crashes with
+#   diff: /home/mernst/.gradle/workers/build/whole-program-inference: No such file or directory
+# Does the directory need to be created first?
+WPIOUTDIR=./build/whole-program-inference/
+
+# The compile and clean commands for the project's build system.
+BUILD_CMD="mvn compile"
+CLEAN_CMD="mvn clean"
+
+# Whether to run in debug mode.
+DEBUG=1
+
+# End of variables. You probably don't need to make changes below this line.
+
+rm -rf ${WPITEMPDIR}
+mkdir -p ${WPITEMPDIR}
+
+while : ; do
+    if [[ ${DEBUG} == 1 ]]; then
+	echo "entering a new iteration"
+    fi
+    ${BUILD_CMD}
+    ${CLEAN_CMD}
+    # This mkdir is needed when the project has subprojects.
+    mkdir -p "${WPITEMPDIR}"
+    mkdir -p "${WPIOUTDIR}"
+    [[ $(diff -r ${WPITEMPDIR} ${WPIOUTDIR}) ]] || break
+    rm -rf ${WPITEMPDIR}
+    mv ${WPIOUTDIR} ${WPITEMPDIR}
+done
